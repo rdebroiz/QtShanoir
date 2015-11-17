a0b7d54634a6b9af1d787b656be58b176dbcc6b3
René-paul Debroize <rene-paul.debroize@irisa.fr>
2015-11-17 15:48:21 +0100

    add missing files.

---
+++ Command/shanoirqr/main.cpp
@@ -0,0 +1,126 @@
+#include <docopt.h>
+#include <shanoirqr.h>
+
+#include <QCoreApplication>
+#include <QString>
+#include <QTimer>
+#include <QFile>
+#include <QIODevice>
+#include <QTextStream>
+#include <QSettings>
+#include <QDebug>
+
+#include <iostream>
+
+void shMessageHandler(QtMsgType type, const char *msg)
+{
+    QString txt;
+    switch (type) {
+    case QtDebugMsg:
+        txt = QString("Debug: %1").arg(msg);
+        break;
+    case QtWarningMsg:
+        txt = QString("Warning: %1").arg(msg);
+    break;
+    case QtCriticalMsg:
+        txt = QString("Critical: %1").arg(msg);
+    break;
+    case QtFatalMsg:
+        txt = QString("Fatal: %1").arg(msg);
+        abort();
+    }
+    QFile outFile("shanoirqr.log");
+    outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream ts(&outFile);
+    ts << txt << endl;
+}
+
+static const char USAGE[] =
+R"(shanoirqr
+
+    Usage:
+      shanoirqr (-i <ini_file> | <login> <password> <host> [--port <port_number>] [--truststore <things>]) [-q <level:filter>]... -d <path>
+      shanoirqr (-h | --help)
+      shanoirqr --version
+
+    Options:
+      -h --help            Show this screen.
+      --port port_number   Port used to be connected to the shanoir server [default: 0]
+      --truststore things  Truststore thing [default: ""]
+      -i                   ini configuration file for all connection stuff. [default: ""]
+      -q level:filter      Querry to send to shanoir, must look likes [level:filter], level beeing in [study, patient, exam, dataset]
+      -d path              Downlaod dataset found to path [default: ""]
+      --version            Show version.
+)";
+
+int main(int argc, const char** argv)
+{
+    QCoreApplication app(argc, const_cast<char **>(argv));
+    qInstallMsgHandler(shMessageHandler);
+
+    std::map<std::string, docopt::value> args
+        = docopt::docopt(USAGE,
+                         { argv + 1, argv + argc },
+                         true,               // show help if requested
+                         "shanoirqr 1.a");  // version string
+
+    for(auto const& arg : args)
+    {
+        std::cout << arg.first << ": " << arg.second << std::endl;
+    }
+    std::cout << std::endl << std::endl;
+
+    arguments a;
+
+    if(! args["<ini_file>"].asString().empty())
+    {
+        QString settings_filename = QString::fromUtf8(args["<ini_file>"].asString().c_str());
+        QSettings settings(settings_filename, QSettings::IniFormat);
+
+        settings.beginGroup("Users");
+        a.login = settings.value("login", "").toString();
+        a.password = settings.value("password", "").toString();
+        settings.endGroup();
+
+        settings.beginGroup("Server");
+        a.host = settings.value("host", "").toString();
+        a.port = settings.value("port", "").toInt();
+        settings.endGroup();
+
+        settings.beginGroup("Security");
+        a.truststore = settings.value("truststore", "").toString();
+        settings.endGroup();
+    }
+    else
+    {
+        a.login = QString::fromUtf8(args["<login>"].asString().c_str());
+        a.password = QString::fromUtf8(args["<password>"].asString().c_str());
+        a.host = QString::fromUtf8(args["<host>"].asString().c_str());
+        a.login = atoi(args["<port>"].asString().c_str());
+        a.truststore = QString::fromUtf8(args["<truststore>"].asString().c_str());
+    }
+
+    if(args["-q"].isStringList())
+    {
+        for(auto const& querry : args["-q"].asStringList())
+        {
+            a.filter << QString::fromUtf8(querry.c_str());
+        }
+    }
+    if(args["-d"].isString())
+        a.download = QString::fromUtf8(args["-d"].asString().c_str());
+
+    qDebug() << "login: " << a.login;
+    qDebug() << "password: " << a.password;
+    qDebug() << "host: " << a.host;
+    qDebug() << "port: " << a.port;
+    qDebug() << "truststore: " << a.truststore;
+
+    shanoirqr *task = new shanoirqr(&app);
+    task->setArgs(a);
+
+    QObject::connect(task, SIGNAL(finished()), &app, SLOT(quit()));
+    QTimer::singleShot(0, task, SLOT(start()));
+
+    app.exec();
+}
---
+++ Command/shanoirqr/shanoirqr.cpp
@@ -0,0 +1,112 @@
+#include <shanoirqr.h>
+
+#include <iostream>
+
+#include <dao.h>
+
+
+shanoirqr::shanoirqr(QObject *parent): QObject(parent)
+{
+
+}
+shanoirqr::~shanoirqr()
+{
+
+}
+void shanoirqr::start()
+{
+    GlobalData::settings.setLogin(m_args.login);
+    GlobalData::settings.setPassword(m_args.password);
+    GlobalData::settings.setHost(m_args.host);
+    GlobalData::settings.setPort(m_args.port);
+    GlobalData::settings.setTrustore(m_args.truststore);
+
+    authentification();
+
+    if(m_args.filter.isEmpty())
+    {
+        QMap<int, QString> studies = findStudyList("");
+        for(int key : studies.keys())
+        {
+            std::cout  << "Study name:" << studies[key].toStdString() << "\t|\tdb key: "<< key << std::endl;
+        }
+    }
+    else
+    {
+        QStringList studyFilterList, patientFilterList, examFilterList, datasetFilterList;
+        for(QString filter : m_args.filter)
+        {
+            QString lvl = filter.split(":")[0];
+
+            if(lvl == "study")
+                studyFilterList << filter.split(":")[1];
+            else if(lvl == "patient")
+                patientFilterList << filter.split(":")[1];
+            else if(lvl == "exam")
+                examFilterList << filter.split(":")[1];
+            else if(lvl == "dataset")
+                datasetFilterList << filter.split(":")[1];
+        }
+
+        if(studyFilterList.isEmpty())
+            studyFilterList << "";
+        if(patientFilterList.isEmpty())
+            patientFilterList << "";
+        if(examFilterList.isEmpty())
+            examFilterList << "";
+        if(datasetFilterList.isEmpty())
+            datasetFilterList << "";
+
+        for(QString studyfilter : studyFilterList)
+        {
+            QMap<int, QString> studiesMap;
+            studiesMap.unite(findStudyList(studyfilter));
+            for(int study : studiesMap.keys())
+            {
+                std::cout  << "Study name:" << studiesMap[study].toStdString() << "\t|\tdb key: "<< study << std::endl;
+                QMap<int, QString> patientsMap;
+                for(QString patientFilter : patientFilterList)
+                {
+                    patientsMap.unite(findSubjectList(studiesMap[study], patientFilter));
+                    for(int patient : patientsMap.keys())
+                    {
+                        std::cout  << "\tPatient name:" << patientsMap[patient].toStdString() << "\t|\tdb key: "<< patient << std::endl;
+                        QMap<int, QString> examMap;
+                        for(QString examFilter : examFilterList)
+                        {
+                            examMap.unite(findExamListf(study, patient, examFilter));
+                            for(int exam : examMap.keys())
+                            {
+                                std::cout  << "\t\tExam name:" << examMap[exam].toStdString() << "\t|\tdb key: "<< exam << std::endl;
+                                QMap<int, QString> datasetMap;
+                                for(QString datasetFilter : datasetFilterList)
+                                {
+                                    datasetMap.unite(findDatasetListf(study, patient, exam, datasetFilter));
+                                    for(int dataset : datasetMap.keys())
+                                    {
+                                        std::cout  << "\t\tDataset name:" << datasetMap[dataset].toStdString() << "\t|\tdb key: "<< dataset << std::endl;
+                                        if(!m_args.download.isEmpty())
+                                        {
+//                                            QString filename = m_args.download +  QString::number(study) + "_" +
+//                                                               QString::number(patient) + "_" +
+//                                                               QString::number(exam) + "_" +
+//                                                               + dataset;
+                                            downloadFile(datasetMap[dataset], m_args.download);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    emit finished();
+
+}
+void shanoirqr::setArgs(arguments const& args)
+{
+    m_args = args;
+}
